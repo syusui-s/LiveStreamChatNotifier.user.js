6ff50b9a438adc16946088e1ad1ea88ee39fe79f
Shusui MOYATANI <syusui.s@gmail.com>
2018-08-07 11:03:35 +0900

    現状コミット

--- settings/YouTubeCommentNotifierSettings.js
+++ settings/YouTubeCommentNotifierSettings.js
@@ -1,6 +1,31 @@
 window.addEventListener('DOMContentLoaded', async () => {
   'use strict';
 
+  class Enum {
+    constructor(set) {
+      for (const entry of set) {
+        const entryStr = entry.toString();
+        this[entryStr] = Symbol(entryStr);
+      }
+
+      Object.freeze(this);
+
+      return new Proxy(this, {
+        get(target, name) {
+          if (name in target) {
+            return target[name];
+          }
+
+          throw new TypeError(`No such enum property: ${name}`);
+        },
+      });
+    }
+
+    includes(item) {
+      return Object.values(this).includes(item);
+    }
+  }
+
   const html = (callSites, ...substitutions) => {
     const escapedSubstitutions = substitutions.map(value =>
       value.toString().replace(/[&'`"<>]/g, match => ({
@@ -52,31 +77,57 @@ window.addEventListener('DOMContentLoaded', async () => {
     return elem;
   };
 
-  const channelNameView = channelName => html`
-    <li class="channelName">
-      <div class="channelName__name">${channelName}</div>
-      <div class="channelName__menu">
-        <button class="channelName__edit">編集</button>
-        <button class="channelName__delete">削除</button>
-      </div>
-    </li>
-  `;
+  const channelNameView = channelName => {
+    const elem = html`
+      <li class="channelName">
+        <div class="channelName__name">${channelName}</div>
+        <div class="menu">
+          <button class="menu__item channelName__edit">編集</button>
+          <button class="menu__item channelName__delete">削除</button>
+        </div>
+      </li>
+    `;
 
+    elem.querySelector('channelName__edit')
+      .addEventListener('click', () => emit(actions.showEdit(channelName)));
+    elem.querySelector('channelName__delete')
+      .addEventListener('click', () => emit(actions.deleteConfirm(channelName)));
+
+    return elem;
+  };
+
+  const states = new Enum([ 'Initial', 'Adding', 'Editing', ]);
+
+  const actions = {
+    showEdit(channelName) {
+      Object.assign(state, {
+        current: 
+      });
+    }
+  };
 
   const storageUrl = new URL(`${window.location.origin}/settings/storage.html`);
   const remoteStorage = await RemoteStorage.create(storageUrl);
   const repository = new YouTubeSettingsRepository(remoteStorage);
 
+  let settings;
   try {
-    const settings = await repository.getSettings() || YouTubeSettings.default();
-    const root = document.body.querySelector('.root');
-    root.appendChild(settingsView(settings));
+    settings = await repository.getSettings() || YouTubeSettings.default();
   } catch (e) {
     window.alert([
       '設定の読み込みに失敗しました。',
       '通常と異なるページで動作させていませんか？',
       'https://github.com/syusui-s/YouTubeCommentNotifier.user.js/ から設定してみてください',
     ].join('\n'));
+
+    return;
   }
+
+  const state = {
+    current: states.Initial,
+  };
+
+  const root = document.body.querySelector('.root');
+  root.appendChild(settingsView(settings));
 });
 
